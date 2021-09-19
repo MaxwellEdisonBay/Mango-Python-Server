57844ee0e22e385ffc44297e12bbfefd501698a7
ivan <ivanteslenkopraha@gmail.com>
2021-09-19 18:14:46 +0200

    flask app modified

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self,name, test):
+        return {"data":f"Hello World You name is {name} and age is {test}"}
+    def post(self):
+        return {"data": "Posted"}
+
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
